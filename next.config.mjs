63c871c41ff2e28a3ec2c69722c7221e194a263a
riccardi1997 <riccardi.salvatore.97@gmail.com>
2024-03-27 18:05:17 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/",
-    output: "export",
-    reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
