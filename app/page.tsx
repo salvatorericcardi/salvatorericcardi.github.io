160e31510a1831dcb166eb6faba027fa6cbfcba0
riccardi1997 <riccardi.salvatore.97@gmail.com>
2024-03-29 17:29:28 +0100

    fix homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
-    <main id="main" className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+    <main id="main" className="flex min-h-screen flex-col items-center p-24 gap-y-6">
+      <div className="flex flex-col justify-between z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex-row md:flex-row">
+        <div className="flex flex-col w-auto justify-center border border-gray-200 from-zinc-200 p-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:flex-row lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:bg-gradient-to-b lg:p-4 lg:dark:bg-zinc-800/30 md:flex-row md:bg-transparent md:border md:bg-gray-200 md:p-4 md:rounded-xl md:border md:dark:bg-zinc-800/30">
           Salvatore Riccardi&nbsp;
-          <code className="font-mono font-bold">My Own Website</code>
+          <code className="font-mono font-bold text-center">My Own Website</code>
         </div>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="hidden w-auto pb-6 pt-8 lg:static lg:h-auto lg:w-auto lg:p-4 lg:bg-none lg:block md:p-4 md:block">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 font-bold lg:pointer-events-auto lg:p-4"
+            className="pointer-events-none p-8 font-bold lg:pointer-events-auto lg:p-4 md:p-4"
             href="https://www.linkedin.com/in/salvatore-riccardi/"
             target="_blank"
           >
@@ -16,29 +16,29 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center text-center my-6">
         <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
           <h2 className="mb-3 text-2xl font-semibold">Educational Projects</h2>
         </div>
-        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
             href="https://salvatorericcardi.github.io/alpaca-generator"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
+            <h2 className="mb-3 text-2xl font-semibold">
               Alpaca Image Generator{" "}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 &rarr;
               </span>
             </h2>
             <div className="flex flex-row gap-x-2 mb-3 text-xs opacity-50">
-              <span className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-3 pt-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-2 lg:dark:bg-zinc-800/30">
+              <span className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 p-2 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30">
                 html
               </span>
-              <span className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-3 pt-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-2 lg:dark:bg-zinc-800/30">
+              <span className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 p-2 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30">
                 css
               </span>
-              <span className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-3 pt-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-2 lg:dark:bg-zinc-800/30">
+              <span className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 p-2 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:dark:bg-zinc-800/30">
                 javascript
               </span>
             </div>
@@ -49,7 +49,7 @@ export default function Home() {
         </div>
       </div>
       <div id="contacts" className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="lg:p-4">
+        <div className="text-center lg:p-4 lg:text-start md:text-start">
           Proudly based in <strong>Fondi</strong>
         </div>
         <div className="hidden fixed bottom-0 left-0 items-center justify-center aspect-square h-48 w-full bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
