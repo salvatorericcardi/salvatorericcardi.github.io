27329d61953d3c68a136436cdce96decad7b1ab7
riccardi1997 <riccardi.salvatore.97@gmail.com>
2024-04-12 18:22:03 +0200

    fix: get-off sitemap from .gitignore

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 export default {
     siteUrl: process.env.SITE_URL || "https://salvatorericcardi.github.io",
+    changefreq: 'yearly',
+    priority: 1.0,
+    lastmod: new Date().toISOString(),
+    generateIndexSitemap: false,
     additionalPaths: async () => [
         {
             loc: "https://salvatorericcardi.github.io/alpaca-generator",
@@ -8,5 +12,5 @@ export default {
             priority: 0.7,
             lastmod: new Date().toISOString(),
         }
-    ]
+    ],
 }
