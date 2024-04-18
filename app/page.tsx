01f48964906d70a57ab6114a86e8756345417d58
riccardi1997 <riccardi.salvatore.97@gmail.com>
2024-04-18 18:36:27 +0200

    fix: useEffect needed for window object

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 'use client';
 import { i18n } from "@/i18n.config";
+import { useEffect } from "react";
 
 export default function Home(): void {
-  window.location.href = `/${i18n.defaultLocale}`
+  useEffect(() => {
+    window.location.href = `/${i18n.defaultLocale}`
+  }, [])
 }
