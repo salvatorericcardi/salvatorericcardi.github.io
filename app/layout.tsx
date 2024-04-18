b7bdaba7594813f8880c104ffbd42ead38a8612c
riccardi1997 <riccardi.salvatore.97@gmail.com>
2024-04-18 18:22:57 +0200

    fix: restored layout and page in root dir

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import { Inter } from "next/font/google";
+import { Locale, i18n } from "@/i18n.config";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: { lang: Locale };
+}>) {
+  return (
+    <html lang={params.lang ?? i18n.defaultLocale} className="scroll-smooth">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
